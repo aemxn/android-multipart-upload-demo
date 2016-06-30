2d4d90eff7ce47bda1f97122d1990cca3a01f9bd
aimanbaharum <ilovebsod@gmail.com>
2016-06-30 16:08:38 +0800

    Real path

---
+++ app/src/main/java/com/aimanbaharum/camerademo/AnotherActivity.java
@@ -0,0 +1,95 @@
+package com.aimanbaharum.camerademo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by cliqers on 30/6/2016.
+ */
+public class AnotherActivity extends Activity implements View.OnClickListener {
+
+    TextView txtSDK;
+    Button btnSelectImage;
+    TextView txtUriPath,txtRealPath;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        // get reference to views
+        txtSDK = (TextView) findViewById(R.id.txtSDK);
+        btnSelectImage = (Button) findViewById(R.id.btnSelectImage);
+        txtUriPath = (TextView) findViewById(R.id.txtUriPath);
+        txtRealPath = (TextView) findViewById(R.id.txtRealPath);
+        imageView = (ImageView) findViewById(R.id.imgView);
+
+        // add click listener to button
+        btnSelectImage.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent data) {
+        if(resCode == Activity.RESULT_OK && data != null){
+            String realPath;
+            if (Build.VERSION.SDK_INT < 11)
+                realPath = RealPathUtil.getRealPathFromURI_BelowAPI11(this, data.getData());
+            else if (Build.VERSION.SDK_INT < 19)
+                realPath = RealPathUtil.getRealPathFromURI_API11to18(this, data.getData());
+            else
+                realPath = RealPathUtil.getRealPathFromURI_API19(this, data.getData());
+            setTextViews(Build.VERSION.SDK_INT, data.getData().getPath(),realPath);
+        }
+    }
+
+    private void setTextViews(int sdk, String uriPath,String realPath){
+
+        this.txtSDK.setText("Build.VERSION.SDK_INT: "+sdk);
+        this.txtUriPath.setText("URI Path: "+uriPath);
+        this.txtRealPath.setText("Real Path: "+realPath);
+
+        Uri uriFromPath = Uri.fromFile(new File(realPath));
+
+        // you have two ways to display selected image
+
+        // ( 1 ) imageView.setImageURI(uriFromPath);
+
+        // ( 2 ) imageView.setImageBitmap(bitmap);
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uriFromPath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+
+        Log.d("HMKCODE", "Build.VERSION.SDK_INT:"+sdk);
+        Log.d("HMKCODE", "URI Path:"+uriPath);
+        Log.d("HMKCODE", "Real Path: "+realPath);
+    }
+
+}
--- app/src/main/java/com/aimanbaharum/camerademo/MainActivity.java
+++ app/src/main/java/com/aimanbaharum/camerademo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aimanbaharum.camerademo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.DialogInterface;
@@ -8,7 +9,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,16 +16,13 @@ import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.adeel.library.easyFTP;
 
 import java.io.File;
 import java.io.IOException;
@@ -88,69 +85,34 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
 
-            /**
-             * FTP upload
-             */
-//            uploadFtp();
-            new UploadFTP().execute();
-
-
             /** Multipart upload */
-//            try {
-//                execMultipartPost();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
+            try {
+                execMultipartPost();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     };
 
-    private class UploadFTP extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            et_response.setHint("Uploading...");
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-//            File file = new File(selectedImageUri.getPath());
-
-            try {
-                easyFTP ftp = new easyFTP();
-                ftp.connect(Constants.FTP_HOST, Constants.FTP_USER, Constants.FTP_PASSWORD);
-                ftp.setWorkingDirectory("/shoppermate_uploads/");
-                ftp.uploadFile(selectedImageUri.getPath());
-                return "Upload ftp ok";
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "Upload ftp failed. Please see stack trace";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            et_response.setText(s);
-        }
-    }
-
     private void execMultipartPost() throws Exception {
-        File file = new File(selectedImageUri.getPath());
+//        Uri uriFromPath = Uri.fromFile(new File(realPath));
+        File file = new File(realPath);
         String contentType = file.toURL().openConnection().getContentType();
+
+        Log.d(TAG, "file: " + file.getPath());
+        Log.d(TAG, "contentType: " + contentType);
+
+
         RequestBody fileBody = RequestBody.create(MediaType.parse(contentType), file);
 
         final String filename = "file_" + System.currentTimeMillis() / 1000L;
 
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
-                .addFormDataPart("fileUploadType", "1")
-                .addFormDataPart("miniType", contentType)
-                .addFormDataPart("ext", file.getAbsolutePath().substring(file.getAbsolutePath().lastIndexOf(".")))
-                .addFormDataPart("fileTypeName", "img")
-                .addFormDataPart("clientFilePath", selectedImageUri.getPath())
-                .addFormDataPart("filedata", filename + ".png", fileBody)
-                        // timestamp
+                .addFormDataPart("user_id", "1")
+                .addFormDataPart("group_id", "1")
+                .addFormDataPart("token", "NVbk9J_eE@ux2v?3")
+                .addFormDataPart("image", filename + ".jpg", fileBody)
                 .build();
 
         Request request = new Request.Builder()
@@ -181,10 +143,6 @@ public class MainActivity extends AppCompatActivity {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        tv_url.setVisibility(View.VISIBLE);
-                        tv_url.setText(Html.fromHtml("<a href=\"http://api.aimanbaharum.com/shoppermate-test/uploads/" + filename + ".png\">See image in browser</a>"));
-                        tv_url.setMovementMethod(LinkMovementMethod.getInstance());
-                        Toast.makeText(MainActivity.this, "response: " + response, Toast.LENGTH_LONG).show();
                     }
                 });
             }
@@ -238,14 +196,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private static final int REQUEST_GALLERY_KITKAT = 1003;
+
     private void initGalleryIntent() {
-        Intent galleryIntent = new Intent();
-        galleryIntent.setType("image/*");
-        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(galleryIntent, "Select File"),
-                REQUEST_GALLERY
-        );
+        if (Build.VERSION.SDK_INT < 19){
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, REQUEST_GALLERY_KITKAT);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(intent, REQUEST_GALLERY);
+        }
     }
 
     private void initCameraIntent() {
@@ -278,25 +242,29 @@ public class MainActivity extends AppCompatActivity {
         return mediaFile;
     }
 
+    String realPath = "";
 
     @Override
+    @SuppressLint("NewApi")
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == REQUEST_CAMERA) {
-                //
-            } else if (requestCode == REQUEST_GALLERY) {
+                realPath = selectedImageUri.getPath();
+            } else if (requestCode == REQUEST_GALLERY || requestCode == REQUEST_GALLERY_KITKAT) {
                 selectedImageUri = data.getData();
+                if (Build.VERSION.SDK_INT < 11) {
+                    realPath = RealPathUtil.getRealPathFromURI_BelowAPI11(this, data.getData());
+                } else if (Build.VERSION.SDK_INT < 19) {
+                    realPath = RealPathUtil.getRealPathFromURI_API11to18(this, data.getData());
+                } else {
+                    realPath = RealPathUtil.getRealPathFromURI_API19(this, data.getData());
+                }
+
+                Log.d(TAG, "real path: " + realPath);
             }
             mBitmap = ImageUtils.getScaledImage(selectedImageUri, this);
             setImageBitmap(mBitmap);
-//            try {
-//                InputStream inputStream;
-//                inputStream = getContentResolver().openInputStream(selectedImageUri);
-//                mBitmap = BitmapFactory.decodeStream(inputStream);
-//                setImageBitmap(mBitmap);
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//            }
         }
 
     }
